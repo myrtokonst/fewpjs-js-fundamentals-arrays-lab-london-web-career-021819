36b39fc9522a9fadc048adfc53ce4247159cc8c3
Myrto Konstantinidi <myrtokonst@gmail.com>
2019-04-21 09:39:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
 const append = ["Milo", "Otis", "Garfield"]
 append.push("Odie");
 
-const prepend = ["Milo", "Otis", "Garfield"]
+const prepend = ["
+Milo", "Otis", "Garfield"]
 prepend.unshift("Odie");
 
 const removeLast = ["Milo", "Otis", "Garfield"]
